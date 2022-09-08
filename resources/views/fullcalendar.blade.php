4b13e419f37ce17d777e53a9fb71befd4c029c00
ICDVN-DUYENLH\User <duyenlh@icd-vn.com>
2022-09-08 17:05:58 +0700

    update

--- resources/views/fullcalendar.blade.php
+++ resources/views/fullcalendar.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -8,31 +9,92 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.2/fullcalendar.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" />
+    {{-- Scripts --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.17.1/moment.min.js'></script>
+
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.1.0/fullcalendar.min.js'></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+    <script src="https://unpkg.com/moment@2.19.1/min/moment.min.js"></script>
+    <style>
+        .fc-view {
+            overflow-x: auto;
+        }
+
+        .fc-view>table {
+            min-width: 100%;
+        }
+
+        .fc-time-grid .fc-slats {
+            z-index: 4;
+            pointer-events: none;
+        }
+
+        .fc-scroller.fc-time-grid-container {
+            overflow: initial !important;
+        }
+
+        .fc-axis {
+            position: sticky;
+            left: 0;
+            background: white;
+        }
+    </style>
 </head>
+
 <body>
-    <div class="container mt-5" style="max-width: 700px">
+    <div class="container mt-5" >
         <h2 class="h2 text-center mb-5 border-bottom pb-3">Laravel FullCalender CRUD Events Example</h2>
         <div id='full_calendar_events'></div>
     </div>
-    {{-- Scripts --}}
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.2/fullcalendar.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
     <script>
-        $(document).ready(function () {
+        $(document).ready(function() {
             var SITEURL = "{{ url('/') }}";
             $.ajaxSetup({
                 headers: {
                     'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
                 }
             });
+            var calendarEl = document.getElementById('full_calendar_events');
+
             var calendar = $('#full_calendar_events').fullCalendar({
-                editable: true,
                 editable: true,
                 events: SITEURL + "/calendar-event",
                 displayEventTime: true,
-                eventRender: function (event, element, view) {
+                defaultView: 'agendaWeek',
+
+
+                // defaultView: 'resourceTimeGridFourDay',
+                // datesAboveResources: true,
+                // allDaySlot: false,
+                // plugins: ['resourceTimeGrid'],
+                // header: {
+                //     left: 'prev,next',
+                //     center: 'title',
+                //     right: 'resourceTimeGridDay,resourceTimeGridFourDay'
+                // },
+                // views: {
+                //     resourceTimeGridFourDay: {
+                //         type: 'resourceTimeGrid',
+                //         duration: {
+                //             days: 4
+                //         },
+                //         buttonText: '4 days'
+                //     }
+                // },
+                // resources: [{
+                //         id: 'a',
+                //         title: 'Room A'
+                //     },
+                //     {
+                //         id: 'b',
+                //         title: 'Room B'
+                //     }
+                // ],
+                // events: 'https://fullcalendar.io/demo-events.json?with-resources=2',
+
+                eventRender: function(event, element, view) {
                     if (event.allDay === 'true') {
                         event.allDay = true;
                     } else {
@@ -41,11 +103,15 @@
                 },
                 selectable: true,
                 selectHelper: true,
-                select: function (event_start, event_end, allDay) {
+                select: function(event_start, event_end, allDay) {
                     var event_name = prompt('Event Name:');
                     if (event_name) {
-                        var event_start = $.fullCalendar.formatDate(event_start, "Y-MM-DD HH:mm:ss");
-                        var event_end = $.fullCalendar.formatDate(event_end, "Y-MM-DD HH:mm:ss");
+                        console.log(event_start);
+                        
+                        var event_start = moment (event_start).format( "Y-MM-DD HH:mm:ss");
+                        console.log(event_start);
+                        
+                        var event_end = moment (event_end).format("Y-MM-DD HH:mm:ss");
                         $.ajax({
                             url: SITEURL + "/calendar-crud-ajax",
                             data: {
@@ -55,7 +121,7 @@
                                 type: 'create'
                             },
                             type: "POST",
-                            success: function (data) {
+                            success: function(data) {
                                 displayMessage("Event created.");
                                 calendar.fullCalendar('renderEvent', {
                                     id: data.id,
@@ -69,7 +135,7 @@
                         });
                     }
                 },
-                eventDrop: function (event, delta) {
+                eventDrop: function(event, delta) {
                     var event_start = $.fullCalendar.formatDate(event.start, "Y-MM-DD");
                     var event_end = $.fullCalendar.formatDate(event.end, "Y-MM-DD");
                     $.ajax({
@@ -82,12 +148,12 @@
                             type: 'edit'
                         },
                         type: "POST",
-                        success: function (response) {
+                        success: function(response) {
                             displayMessage("Event updated");
                         }
                     });
                 },
-                eventClick: function (event) {
+                eventClick: function(event) {
                     var eventDelete = confirm("Are you sure?");
                     if (eventDelete) {
                         $.ajax({
@@ -97,7 +163,7 @@
                                 id: event.id,
                                 type: 'delete'
                             },
-                            success: function (response) {
+                            success: function(response) {
                                 calendar.fullCalendar('removeEvents', event.id);
                                 displayMessage("Event removed");
                             }
@@ -105,10 +171,13 @@
                     }
                 }
             });
+
         });
+
         function displayMessage(message) {
-            toastr.success(message, 'Event');            
+            toastr.success(message, 'Event');
         }
     </script>
 </body>
+
 </html>
